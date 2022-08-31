aed29d760d28440f670880e122b16f6f9a1676f1
Bello Damilola <damilolabells@gmail.com>
2022-08-31 11:09:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Task for Flexisaf trainee UI/UX
+
+I translated my design only on mobile view
+max-width 600px
---
+++ index.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("myLink");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myFunction3() {
+    var x = document.getElementById("Links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
